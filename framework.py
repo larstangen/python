f7eb5a3d8edcd874a2f6c3a622c96c4d5b775b4a
Lars Tangen <ltangen26@gmail.com>
2018-03-23 23:16:35 -0700

    added connection string

--- framework.py
+++ framework.py
@@ -2,11 +2,16 @@ import sys
 import cx_Oracle
 import getpass
 
-#if __name__ == "__main__":
-#   print(sys.argv[1])
-
+instance = input('instance [prd,stg,tst,dv1,etc.]:')
+#print(instance)
 username = input('username:')
-print(username)
+#print(username)
 #password = input('password:')
 pswd = getpass.getpass('password:')
-print(pswd)
+#print(pswd)
+constring = '\'' + username + '/' + pswd + "@" + 'instance' + '\''
+#con = cx_Oracle.connect(constring)
+print(constring)
+
+#if __name__ == "__main__":
+#   print(sys.argv[1])
